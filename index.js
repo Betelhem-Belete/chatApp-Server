bec64dcd3641e9e3931c6f67cda11410b2315cfd
Betelhem-Belete <betelhembelete0@gmail.com>
2023-11-19 07:27:39 -0500

    ignore nodemodules

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const jwt = require('jsonwebtoken');
+const User = require('./models/user');
+const cors = require('cors');
+
+dotenv.config();
+// console.log(process.env.MONGO_URL)
+mongoose.connect(process.env.MONGO_URL);
+const jwtSecret = process.env.JWT_SECRET;
+
+const app = express();
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.CLIENT_URL,
+  })
+);
+
+app.get('/t', (req, res) => {
+  res.json('test ok');
+});
+
+app.post('/Register', async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const createdUser = await User.create({ username, password });
+    jwt.sign({ userId: createdUser._id }, jwtSecret, {}, (err, token) => {
+      if (err) throw err;
+      res.cookie('token', token).status(201).json('ok');
+    });
+  } catch (err) {
+    if (err) throw err;
+  }
+});
+
+app.listen(4000);
